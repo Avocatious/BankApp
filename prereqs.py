70347e88af5b40b6ff047b40e94adf56cf6cdebd
WannabeDeveloper123 <vishal.murali.kannan@gmail.com>
2025-03-08 17:30:42 -0800

    included transfer fn

--- prereqs.py
+++ prereqs.py
@@ -1,5 +1,5 @@
 from sys import float_repr_style
-#meow
+# #meow
 class User:
     def __init__(self, name: str, bal: float, debt: float, cscore: float):
         self.name = name
@@ -8,58 +8,83 @@ class User:
         self.cscore = cscore
 
     def withdraw(self, withdrawal: float):
-        if withdrawal > 0:
-            print("Enter valid withdrawal amount.")
+        if withdrawal <= 0:
+            print("Enter amount greater than 0.")
+            return
+        if withdrawal > self.balance:
+            excess = withdrawal - self.balance
+            self.debt += excess
+            self.balance = 0
         else:
-            for i in range(0, withdrawal):
-                self.balance -= 1
-                if self.balance == 0:
-                    self.debt += 1
-                    self.cscore -= 0.002 * withdrawal
-            return "Balance: {}, Debt: {}".format(self.balance, self.debt)
+            self.balance -= withdrawal
+        self.cscore -= 0.002 * withdrawal
+        print("Withdrawal successful! Balance: {}, Debt: {}".format(self.balance, self.debt))
 
     def deposit(self, deposit: float):
-        for i in range(0, deposit):
-            self.balance += 1
-            self.cscore += 0.003 * deposit
-        return "Balance: {}, Current Credit Score: {}".format(self.balance, self.cscore)
+        if deposit <= 0:
+            print("Enter a valid deposit amount.")
+            return
+        self.balance += deposit
+        self.cscore += 0.003 * deposit
+        print("Deposit successful. New Balance: {}".format(self.balance))
 
     def view_cscore(self):
         return self.cscore
 
+    def transfer(self, amount: float, recipient):
+        if amount <= 0:
+            print("Transfer amount must be greater than 0.")
+            return False
+        if self.balance + self.debt < amount:
+            print("Insufficient funds for transfer!")
+            return False
+        #Transfer facilitation with debt
+        if amount > self.balance:
+            extra = amount - self.balance
+            self.debt += extra
+            self.balance = 0
+        else:
+            self.balance -= amount
+
+        recipient.balance += amount
+        return True
+
 
 class Backend:
-    def __init__(self, fname = 'Bankusers.txt'):
+    def __init__(self, fname='Bankusers.txt'):
         self.userdb = {}
         self.fname = fname
         self.load_user()
 
-    def saving_users(self):
+    def save(self):
         with open(self.fname, 'w+') as fw:
             for user in self.userdb.values():
                 fw.write(f"{user.name},{user.balance},{user.debt},{user.cscore}\n")
-            print("User{} saved to {}.".format(User.name, self.fname))
 
-    def load_user(self, uname):
-        with open(self.fname, 'r') as fr:
-            for line in fr.readlines():
-                acc = line.strip().split(',')
-                name = acc[0]
-                if name == uname:
+    def load_user(self):
+        try:
+            with open(self.fname, 'r') as fh:
+                for line in fh.readlines():
+                    acc = line.strip().split(',')
+                    name = acc[0]
                     balance = float(acc[1])
                     debt = float(acc[2])
                     cscore = float(acc[3])
                     user = User(name, balance, debt, cscore)
                     self.userdb[name] = user
-                    return user
+            print("*****************PyBank Customers*****************")
+            return self.userdb
+        except FileNotFoundError:
+            print("Database not found.")
 
     def create_user(self, uname):
         if uname in self.userdb:
-            print(uname, "already exists!")
+            print("Account {} already exists!".format(uname))
             return
         balance = float(input("Enter balance to deposit: "))
-        debt, cscore = 0, 0
+        debt, cscore = 0, 700
         self.userdb[uname] = User(uname, balance, debt, cscore)
+        print("Account for {} created.".format(uname))
 
     def depositer(self):
         uname = input("Enter account holder name to deposit money in: ")
@@ -70,7 +95,7 @@ class Backend:
             print("Account not found.")
 
     def withdrawer(self):
-        uname = input("Enter account holder name to withdraw money in: ")
+        uname = input("Enter account holder name to withdraw money from: ")
         if uname in self.userdb:
             witd = float(input("Enter amount to withdraw: "))
             self.userdb[uname].withdraw(witd)
@@ -80,36 +105,26 @@ class Backend:
     def cscore_viewer(self):
         uname = input("Enter account holder name to view credit score: ")
         if uname in self.userdb:
-            self.userdb[uname].cscore_viewer()
+            print("{}'s credit score: {}".format(uname, self.userdb[uname].view_cscore()))
         else:
             print("Account not found.")
 
-def main():
-    backend = Backend()
-    while True:
-        print("*****Welcome to PyBank!*****")
-        print("1. Create Account")
-        print("2. View Account")
-        print("3. Deposit Money")
-        print("4. Withdraw Money")
-        print("5. Check Credit Score")
-        print("6. Exit")
-        opt = int(input("Choose an option: "))
-        if opt == 1:
-            uname = input("Enter Username: ")
-            backend.create_user(uname)
-        elif opt == 2:
-            uname = input("Enter Username: ")
-            backend.load_user(uname)
-        elif opt == 3:
-            backend.depositer()
-        elif opt == 4:
-            backend.withdrawer()
-        elif opt == 5:
-            backend.cscore_viewer()
-        elif opt == 6:
-            backend.saving_users()
-        else:
-            print("Enter valid option.")
+    def transfer_money(self):
+        sender = input("Enter sender's account name: ")
+        recipient  = input("Enter recipient's account name: ")
+        if sender not in self.userdb or recipient not in self.userdb:
+            print("Either name does not exist in PyBank.")
+            return
+        sinfo = self.userdb[sender]
+        rinfo = self.userdb[recipient]
+        try:
+            amount = float(input("Enter the amount to transfer: "))
+            if amount <= 0:
+                print("The amount must be greater than 0.")
+                return
+            if sinfo.transfer(amount, rinfo) == True:
+                print("Transfer of {} from {} to {} successful!".format(amount, sender, recipient))
+        except ValueError:
+            print("Invalid amount entered.")
 
 
--- ui.py
+++ ui.py
@@ -3,29 +3,43 @@ from prereqs import User, Backend
 def main():
     backend = Backend()
     while True:
-        print("*****Welcome to PyBank!*****")
+        print("Welcome to PyBank!:")
         print("1. Create Account")
-        print("2. View Account")
-        print("3. Deposit Money")
-        print("4. Withdraw Money")
-        print("5. Check Credit Score")
+        print("2. Deposit Money")
+        print("3. Withdraw Money")
+        print("4. View Credit Score")
+        print("5. Transfer Money")
         print("6. Exit")
-        opt = int(input("Choose an option: "))
-        if opt == 1:
-            uname = input("Enter Username: ")
+        choice = input("What would you like to do today? (Choose a number): ")
+
+        if choice == '1':
+            uname = input("Enter account holder name: ")
             backend.create_user(uname)
-        elif opt == 2:
-            uname = input("Enter Username: ")
-            backend.load_user(uname)
-        elif opt == 3:
+            backend.save()
+        elif choice == '2':
             backend.depositer()
-        elif opt == 4:
+            backend.save()
+        elif choice == '3':
             backend.withdrawer()
-        elif opt == 5:
+            backend.save()
+        elif choice == '4':
             backend.cscore_viewer()
-        elif opt == 6:
-            backend.saving_users()
+        elif choice == '5':
+            backend.transfer_money()
+            backend.save()
+        elif choice == '6':
+            print("Thank you for using PyBank!")
+            break
+        elif choice.lower() == 'admin':
+            db = backend.load_user()
+            if db:
+                for uname, user in db.items():
+                    print("User: {}, Balance: {}, Debt: {}, Credit Score: {}".format(uname, user.balance, user.debt, user.cscore))
+            else:
+                print("Empty Database.")
         else:
-            print("Enter valid option.")
+            print("Invalid choice. Input can only be a number from 1 through 6.")
+
+
 if __name__ == "__main__":
     main()
